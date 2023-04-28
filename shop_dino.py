9a7f12119ab5c8b9c7200fadc3138da3d2a8a025
DaenenTDD <44416985+DaenenTDD@users.noreply.github.com>
2023-04-28 15:30:26 -0600

    Revert "Added  broken functionality to button 1"
    This reverts commit 8905ae8de95241d27eefb29c9f9e0d372dec4e38.

--- shop_dino.py
+++ shop_dino.py
@@ -1,5 +1,4 @@
 import pygame
-import os
 
 pygame.init()
 
@@ -9,32 +8,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (180, 180, 180)
 
-def buy_box():
-    global total_points, box1_bought, dino_jump_vel
-    if not box1_bought and total_points >= 500:
-        total_points -= 500
-        box1_bought = True
-        dino_jump_vel = -25
-        save_box1_bought()
-
-def save_box1_bought():
-    with open(box1_path, 'w') as file:
-        file.write(str(box1_bought))
-
-def file_exists(file_path):
-    return os.path.exists(file_path)
-
-def load_box1_bought():
-    global box1_bought
-    path = os.path.dirname(os.path.abspath(__file__))
-    box1_path = os.path.join(path, 'res', 'box1.txt')
-    if os.path.exists(box1_path):
-        with open(box1_path, 'r') as f:
-            box1_bought = f.read().strip() == 'True'
-    else:
-        box1_bought = False
-
-def shop_gui(screen, total_points, box1_path):
+def shop_gui(screen, total_points):
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Dino Shop')
 
@@ -57,14 +31,6 @@ def shop_gui(screen, total_points, box1_path):
         boxes.append(box)
         box_rects.append(box_rect)
 
-    box1_text = font.render('Box 1', True, BLACK)
-    box1_price_text = font.render('500 points', True, BLACK)
-    box1_bought_text = font.render('Bought', True, BLACK)
-    box1_rect = pygame.Rect(0, 0, box_width, box_height)
-    box1_rect.x = 0
-    box1_rect.y = HEIGHT - box_height
-    box1_bought = load_box1_bought()
-
     def draw_total_points_shop():
         text = font.render(f'Total Points: {total_points}', True, BLACK)
         screen.blit(text, (WIDTH - text.get_width() - 10, 10))
@@ -76,12 +42,6 @@ def shop_gui(screen, total_points, box1_path):
 
         screen.blit(exit_text, (WIDTH // 2 - exit_text.get_width() // 2, 10))
 
-        if box1_bought:
-            screen.blit(box1_bought_text, (box1_rect.x + box_width // 2 - box1_bought_text.get_width() // 2, box1_rect.y + box_height // 2 - box1_bought_text.get_height() // 2))
-        else:
-            screen.blit(box1_text, (box1_rect.x + box_width // 2 - box1_text.get_width() // 2, box1_rect.y + box_height // 2 - box1_text.get_height() // 2))
-            screen.blit(box1_price_text, (box1_rect.x + box_width // 2 - box1_price_text.get_width() // 2, box1_rect.y + box_height - box1_price_text.get_height()))
-        
         for box in boxes:
             screen.blit(box, (boxes.index(box) * (box_width + 2), HEIGHT - box_height))
 
@@ -94,10 +54,10 @@ def shop_gui(screen, total_points, box1_path):
             if event.type == pygame.QUIT:
                 running = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
-                if box1_rect.collidepoint(mouse_pos):
-                    buy_box()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    back_to_death_screen = True
+                    running = False
 
         pygame.display.update()
 
@@ -106,5 +66,4 @@ def shop_gui(screen, total_points, box1_path):
 if __name__ == "__main__":
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     total_points = 0
-    box1_path = 'res/box1.txt'
-    shop_gui(screen, total_points, box1_path)
+    shop_gui(screen, total_points)
